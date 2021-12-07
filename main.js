1cf89242fe6fb814968e2b1d61b5f9e836abdb47
Joaquin Cornelatti <Darkjoa.jc@gmail.com>
2021-12-06 22:24:51 -0300

    Quinto Commit

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 //Cambiando texto en "Sobre mi"
-let btnGet = document.querySelector('#btn-get');
+document.getElementById("Label1").style.display='none';
+document.getElementById("Label2").style.display='none';
+document.getElementById("Label3").style.display='none';
 let inputGet = document.querySelector('#input-get');
 let result = document.querySelector('#result');
-btnGet.addEventListener('click', () =>{
+inputGet.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
     result.innerText = inputGet.value;
+    document.getElementById("Label1").style.display="none";
+  }
+    
 });
 //Cambiando texto en Estudios:
-let btnGet2 = document.querySelector('#btn-get2');
+
 let inputGet2 = document.querySelector('#input-get2');
 let result2 = document.querySelector('#result2');
 
-btnGet2.addEventListener('click', () =>{
+inputGet2.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
     result2.innerText = inputGet2.value;
+    document.getElementById("Label2").style.display="none";
+  }
+    
 });
 
 const image_input = document.querySelector("#image_input");
@@ -29,10 +39,51 @@ image_input.addEventListener('change', function() {
 });
 
 //Cambiando texto en Experiencia:
-let btnGet3 = document.querySelector('#btn-get3');
+
 let inputGet3 = document.querySelector('#input-get3');
 let result3 = document.querySelector('#result3');
 
-btnGet3.addEventListener('click', () =>{
+inputGet3.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
     result3.innerText = inputGet3.value;
+    document.getElementById("Label3").style.display="none";
+  }
+    
 });
+
+var a;
+function show_hide1(){
+  
+  if (a==1){
+    document.getElementById("Label1").style.display='none';
+    return a=0;
+  }
+  else {
+    document.getElementById("Label1").style.display='block';
+    return a=1;
+  }
+}
+var b;
+function show_hide2(){
+  
+  if (b==1){
+    document.getElementById("Label2").style.display='none';
+    return b=0;
+  }
+  else {
+    document.getElementById("Label2").style.display='block';
+    return b=1;
+  }
+}
+var c;
+function show_hide3(){
+  
+  if (c==1){
+    document.getElementById("Label3").style.display='none';
+    return c=0;
+  }
+  else {
+    document.getElementById("Label3").style.display='block';
+    return c=1;
+  }
+}
